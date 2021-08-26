90041927676797710c92f6b198364e7d8196ca86
kai.tao <kai.tao@inveno.com>
2021-08-26 10:13:49 +0800

    ip

--- picServer.py
+++ picServer.py
@@ -24,7 +24,7 @@ def list_pictures(url):
     return files
 
 lock = Lock()
-ip_address = "http://localhost:9092/"
+ip_address = "http://192.168.1.249:9092/"
 file_cursor = 0
 total_picture_files = list_pictures(ip_address)
 labeled_pictures = []
